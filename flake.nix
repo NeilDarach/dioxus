2e368008b956cde79f4595f09d6ebfd1dab3b8d7
Neil Darach <neil.darach@gmail.com>
2025-11-08 08:52:20 +0000

    Tidied up the flake.  The override for cc is now a derivation. Not sure why the stdenv override is necessary, but it is.

--- flake.nix
+++ flake.nix
@@ -1,79 +1,79 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
   };
 
-  outputs = inputs@{ nixpkgs, rust-overlay, darwin, ... }:
+  outputs = inputs@{ nixpkgs, rust-overlay, ... }:
     let
       pkgs = import inputs.nixpkgs {
+        #This is only valid for Mac installations
         system = "aarch64-darwin";
-        #config.replaceStdenv = ({ pkgs }: pkgs.newstdenv);
         overlays = [
           (import rust-overlay)
           (prev: final: {
-            rustToolChain = prev.rust-bin.stable.latest.default.override {
+            rustToolChain = (prev.rust-bin.stable.latest.default.override {
               extensions = [ "rustfmt" "rust-src" ];
               targets = [
                 "wasm32-unknown-unknown"
                 "aarch64-apple-ios"
                 "aarch64-apple-ios-sim"
               ];
-            };
+            }).overrideAttrs (oldAttrs: {
+              ## Modify the rust package to not propagate the wrapped clang packages
+              ## binutils-unwrapped provides install_name_tool which installation needs
+              propagatedBuildInputs = [ pkgs.darwin.binutils-unwrapped ];
+              depsHostHostPropagated = [ ];
+              depsTargetTargetPropagated = [ ];
+            });
           })
-          (self: super:
-            let
-              cc = super.stdenv.cc.override {
-                extraBuildCommands = ''
-                  sed -i '/-mmacos-version-min/d' "''${out}/nix-support/add-flags.sh"
-                  echo "#Modified" >> "''${out}/nix-support/add-flags.sh"'';
-              };
-              newstdenv = super.stdenv.override { inherit cc; };
-            in { inherit newstdenv; })
         ];
       };
-    in {
-      devShells.aarch64-darwin.default = let
-        xcodeenv = import (nixpkgs + "/pkgs/development/mobile/xcodeenv") {
-          inherit (pkgs) callPackage;
-        };
-        bare-rust = pkgs.rustToolChain.overrideAttrs (oldAttrs: {
-          ## don't propagate any build inputs. this allows us to set gcc in stdenv below
-          propagatedBuildInputs = [ pkgs.darwin.binutils-unwrapped ];
-          depsHostHostPropagated = [ ];
-          depsTargetTargetPropagated = [ ];
-        });
-      in (pkgs.mkShellNoCC.override { stdenv = pkgs.llvmPackages.stdenv; }) {
-        strictDeps = true;
 
-        buildInputs = [ ];
-        nativeBuildInputs = [ ];
-        packages = [
-          (xcodeenv.composeXcodeWrapper { })
-          pkgs.just
-          pkgs.bacon
-          pkgs.dioxus-cli
-          bare-rust
-          pkgs.llvmPackages.clang-unwrapped
-        ];
-        shellHook = ''
-          RUST_SRC_PATH="${pkgs.rustToolChain}/lib/rustlib/src/rust/library";
-          PATH="/Users/neil:''${PATH}"
-          unset DEVELOPER_DIR
-          unset SDKROOT
-          #unset MACOSX_DEPLOYMENT_TARGET
-          #NIX_CFLAGS_COMPILE="''${NIX_CFLAGS_COMPILE} -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator26.0.sdk -miphonesimulator-version-min=18.0"
-          #NIX_CFLAGS_COMPILE_FOR_BUILD="''${NIX_CFLAGS_COMPILE_FOR_BUILD} -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator26.0.sdk -miphonesimulator-version-min=18.0"
-
+      #clang-unwrapped provides 'clang' but not 'cc'
+      #without this link, rustc finds the wrapped cc and
+      #that adds macos specific flags which conflict when
+      #cross-compiling for iphone
+      cclink = pkgs.stdenv.mkDerivation {
+        name = "cclink-impure";
+        # Fails in sandbox. Use `--option sandbox relaxed` or `--option sandbox false`.
+        __noChroot = true;
+        buildCommand = ''
+          mkdir -p $out/bin
+          cd $out/bin
+          ln -s "${pkgs.llvmPackages.clang-unwrapped}/bin/clang" cc
         '';
       };
+
+      # Create symlinks to the system XCode installation
+      xcodeenv = import (nixpkgs + "/pkgs/development/mobile/xcodeenv") {
+        inherit (pkgs) callPackage;
+      };
+      xcode = (xcodeenv.composeXcodeWrapper { });
+    in {
+      devShells.aarch64-darwin.default =
+        (pkgs.mkShellNoCC.override { stdenv = pkgs.llvmPackages.stdenv; }) {
+          strictDeps = true;
+
+          packages = with pkgs; [
+            xcode
+            just
+            bacon
+            dioxus-cli
+            rustToolChain
+            llvmPackages.clang-unwrapped
+            cclink
+          ];
+          shellHook = ''
+            RUST_SRC_PATH="${pkgs.rustToolChain}/lib/rustlib/src/rust/library";
+            #Use the system installation of the SDKs, not the nix-installed version
+            unset DEVELOPER_DIR
+            unset SDKROOT
+          '';
+        };
     };
 }
 
