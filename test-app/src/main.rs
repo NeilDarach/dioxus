ce981eaeb5e41b58cbb73943ffd6cbc897801f47
Neil Darach <neil.darach@gmail.com>
2025-11-12 15:27:28 +0000

    Inline styling to rotate one counter

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -5,29 +5,37 @@ fn App() -> Element {
     let mut score_a: Signal<u32> = use_signal(|| 0);
     let mut score_b: Signal<u32> = use_signal(|| 0);
     rsx! {
-        Counter { score: score_a , up: move |i| score_a += i, down: move |(i,v)| if v > 0 { score_a -= i } }
+        Counter { name: "Neil".to_string(), score: score_a , up: move |i| score_a += i, down: move |(i,v)| if v > 0 { score_a -= i } }
         Reset { scores: vec![score_a, score_b], onclick: move |_| { *score_a.write() = 0; *score_b.write() = 0 } }
-        Counter { score: score_b , up: move |i| score_b += i, down: move |(i,v)| if v > 0 { score_b -= i } }
+        div { transform: "rotate(180deg)", Counter { name: "Marion".to_string(), score: score_b , up: move |i| score_b += i, down: move |(i,v)| if v > 0 { score_b -= i } } }
 
     }
 }
 
 #[component]
-fn Counter(score: Signal<u32>, up: EventHandler<u32>, down: EventHandler<(u32, u32)>) -> Element {
+fn Counter(
+    name: String,
+    score: Signal<u32>,
+    up: EventHandler<u32>,
+    down: EventHandler<(u32, u32)>,
+) -> Element {
     rsx! {
-            div { "Counter for {score}"
-            button { onclick: move |_| up.call(10), "+10" }
-            button { onclick: move |_| up.call(5), "+5" }
-            button { onclick: move |_| up.call(1), "+1" }
-            button { onclick: move |_| down.call((1,score())), "-1" }
+           div { "align": "center", "Counter for {name}"
+           br { }
+           "{score}"
+           br { }
+           button { onclick: move |_| up.call(10), "+10" }
+           button { onclick: move |_| up.call(5), "+5" }
+           button { onclick: move |_| up.call(1), "+1" }
+           button { onclick: move |_| down.call((1,score())), "-1" }
     }
-        }
+       }
 }
 
 #[component]
 fn Reset(scores: Vec<Signal<u32>>, onclick: EventHandler<MouseEvent>) -> Element {
     rsx! {
-          div { button { onclick , "Reset" } }
+          div { "align": "center", button { onclick , "Reset" } }
     }
 }
 
