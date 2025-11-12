9243c0d29095e308d89cb0b30c06511d95bfff60
Neil Darach <neil.darach@gmail.com>
2025-11-12 12:48:04 +0000

    Use a separate flake to load dioxus requirements and upgrade to 0.7.1 from a PR repository

--- flake.nix
+++ flake.nix
@@ -3,79 +3,30 @@
     "Provide a devshell suitable for devenv which allows compiling and running dioxus apps in the iPhone simulator provided by the system XCode";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    nixpkgs-dioxus = { url = "github:CathalMullan/nixpkgs/dioxus-cli-v0.7.0"; };
+    dioxus = {
+      url = "github:NeilDarach/flakes?dir=dioxus";
+      #url = "path:./dioxus";
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
-
   };
 
-  outputs = { nixpkgs, rust-overlay, ... }:
+  outputs = { nixpkgs, nixpkgs-dioxus, dioxus, ... }:
     let
       #This is only valid for Mac installations
       system = "aarch64-darwin";
+      dioxus-0-7 = final: prev: {
+        inherit (nixpkgs-dioxus.legacyPackages.${prev.stdenv.hostPlatform.system})
+          dioxus-cli;
+      };
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          (import rust-overlay)
-          (final: prev:
-            let rustToolChain = prev.rust-bin.stable.latest.default;
-            in {
-              rustToolChain = (rustToolChain.override {
-                extensions = [ "rustfmt" "rust-src" ];
-                targets = [
-                  "wasm32-unknown-unknown"
-                  "aarch64-apple-ios"
-                  "aarch64-apple-ios-sim"
-                ];
-              }).overrideAttrs (oldAttrs: {
-                # Modify the rust package to not propagate the wrapped clang package
-                # binutils-unwrapped provides install_name_tool which installation requires
-                propagatedBuildInputs = [ final.darwin.binutils-unwrapped ];
-                depsHostHostPropagated = [ ];
-                depsTargetTargetPropagated = [ ];
-              });
-            })
-          (final: prev: {
-            # Create symlinks to  the system XCode installation
-            system-xcode =
-              (import (nixpkgs + "/pkgs/development/mobile/xcodeenv") {
-                inherit (final) callPackage;
-              }).composeXcodeWrapper { };
-
-            # The default cc points to the wrapped clang from nix.
-            # Without an explicit link to the system compiler, rustc finds
-            # the wrapped compiler and adds macos specific flags which
-            # conflict with the ios flags added by dx
-            # Linking to the system file cribbed from xcodeenv
-            cclink-impure = final.stdenv.mkDerivation {
-              name = "cclink-impure";
-              __noChroot = true;
-              buildCommand = ''
-                mkdir -p $out/bin
-                cd $out/bin
-                ln -s "${final.system-xcode}/bin/clang" cc
-              '';
-            };
-          })
-        ];
+        overlays = [ dioxus-0-7 dioxus.overlays.default ];
       };
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          system-xcode
-          cclink-impure
-          rustToolChain
-          rust-analyzer
-          dioxus-cli
-        ];
-        shellHook = ''
-          export RUST_SRC_PATH="${pkgs.rustToolChain}/lib/rustlib/src/rust/library"
-          # Use the system installation of the SDKs, not the nix-installed versions
-          unset DEVELOPER_DIR
-          unset SDKROOT
-        '';
-      };
+      devShells.${system}.default = let applyDioxus = dioxus.addToShell pkgs;
+      in pkgs.mkShell
+      (applyDioxus { packages = with pkgs; [ rust-analyzer just bacon ]; });
     };
 }
 
