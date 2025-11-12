4e6ed94c80fa438a553b57b143d43f188a187397
Neil Darach <neil.darach@gmail.com>
2025-11-12 09:49:01 +0000

    Include a -1 button to correct mistakes

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -4,32 +4,33 @@ use dioxus::prelude::*;
 fn App() -> Element {
     let mut score_a: Signal<u32> = use_signal(|| 0);
     let mut score_b: Signal<u32> = use_signal(|| 0);
-    rsx! { 
+    rsx! {
         Counter { score: score_a , up: move |i| score_a += i, down: move |(i,v)| if v > 0 { score_a -= i } }
         Reset { scores: vec![score_a, score_b], onclick: move |_| { *score_a.write() = 0; *score_b.write() = 0 } }
         Counter { score: score_b , up: move |i| score_b += i, down: move |(i,v)| if v > 0 { score_b -= i } }
-        
+
     }
 }
 
 #[component]
-fn Counter(score: Signal<u32>, up: EventHandler<u32>, down: EventHandler<(u32,u32)>) -> Element {
+fn Counter(score: Signal<u32>, up: EventHandler<u32>, down: EventHandler<(u32, u32)>) -> Element {
     rsx! {
-        div { "Counter for {score}" 
-        button { onclick: move |e| up.call(10), "+10" }
-        button { onclick: move |e| up.call(5), "+5" }
-        button { onclick: move |e| up.call(1), "+1" }
-}
+            div { "Counter for {score}"
+            button { onclick: move |_| up.call(10), "+10" }
+            button { onclick: move |_| up.call(5), "+5" }
+            button { onclick: move |_| up.call(1), "+1" }
+            button { onclick: move |_| down.call((1,score())), "-1" }
     }
+        }
 }
 
 #[component]
 fn Reset(scores: Vec<Signal<u32>>, onclick: EventHandler<MouseEvent>) -> Element {
-  rsx! {
-      div { button { onclick , "Reset" } }
-}
+    rsx! {
+          div { button { onclick , "Reset" } }
+    }
 }
 
 fn main() {
-dioxus::launch(App);
+    dioxus::launch(App);
 }
