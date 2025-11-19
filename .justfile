1d395518b22f1a2de05741ca65f5644b23cfa496
Neil Darach <neil.darach@gmail.com>
2025-11-19 22:57:33 +0000

    Fixed the flake to actually use the patched source for dioxus-cli. The devshell was still using the locally compiled copy of the cli, and the nix needed quite a bit of extra work to apply the patch set. The justfile no longer needs the patch recipe to copy files as that's done by dx.  Pull the cli package from nixpkgs-unstable instead of the PR repo now that it's landed.

--- .justfile
+++ .justfile
@@ -10,8 +10,3 @@ sim:
 
 build:
     cd test-app && dx bundle --platform ios --release --device true
-    just patch
-
-patch:
-    [[ -d test-app/target/dx/test-app/release ]] && cp 'test-app/assets/AppIcon*.png' test-app/target/dx/test-app/release/ios/TestApp.app || true
-    [[ -d test-app/target/dx/test-app/debug ]] && cp 'test-app/assets/AppIcon*.png' test-app/target/dx/test-app/debug/ios/TestApp.app || true
--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     "Provide a devshell suitable for devenv which allows compiling and running dioxus apps in the iPhone simulator provided by the system XCode";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-dioxus = { url = "github:CathalMullan/nixpkgs/dioxus-cli-v0.7.0"; };
+    nixpkgs-unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
     dioxus = {
       url = "github:NeilDarach/flakes?dir=dioxus";
       #url = "path:./dioxus";
@@ -11,19 +11,33 @@
     };
   };
 
-  outputs = { nixpkgs, nixpkgs-dioxus, dioxus, ... }:
+  outputs = { nixpkgs, nixpkgs-unstable, dioxus, ... }:
     let
       #This is only valid for Mac installations
       system = "aarch64-darwin";
       dioxus-0-7 = final: prev:
         let
+          addRustPatches = pkg: patches: cargoHash:
+            pkg.overrideAttrs (oldAttrs: rec {
+              # take the original source and apply all patches before making it the new source
+              # we cannot use patches or patchPhase because all dependencies are vendored into
+              # a separate derivation before the patch phase resulting in mismatching Cargo.lock
+              # checksums
+              # (Not sure about this, original version specified a new hash, but
+              # simple code updates don't seem to need it.)
+              src = prev.runCommand "patched-source" { } ''
+                cp -v --no-preserve=mode -r ${oldAttrs.src} $out
+                cd $out
+                ${prev.lib.concatMapStringsSep "\n"
+                (patch: ''patch -p1 < "${patch}"'') patches}
+              '';
+              cargoDeps = oldAttrs.cargoDeps.overrideAttrs
+                (prev.lib.const { inherit src; });
+            });
           cli =
-            nixpkgs-dioxus.legacyPackages.${prev.stdenv.hostPlatform.system}.dioxus-cli;
+            nixpkgs-unstable.legacyPackages.${prev.stdenv.hostPlatform.system}.dioxus-cli;
         in {
-          dioxus-cli = cli.overrideAttrs (oldAttrs: {
-            cargoPatches = (oldAttrs.cargoPatches or [ ])
-              ++ [ ./patches/extra_files.patch ];
-          });
+          dioxus-cli = addRustPatches cli [ ./patches/extra_files.patch ] "";
         };
       dioxus-local = final: prev: {
         dioxus-cli = final.stdenv.mkDerivation {
@@ -38,7 +52,7 @@
       };
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ dioxus-local dioxus.overlays.default ];
+        overlays = [ dioxus-0-7 dioxus.overlays.default ];
       };
     in {
       devShells.${system}.default = let applyDioxus = dioxus.addToShell pkgs;
