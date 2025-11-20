9231857dce66072c2e1f628ab4fab8a3b487f826
Neil Darach <neil.darach@gmail.com>
2025-11-20 07:34:12 +0000

    For the simple use-case, just setting patches is fine. Previous failures were probably due to trying to apply a 0.7 patch-set to 0.6 code.

--- flake.nix
+++ flake.nix
@@ -15,44 +15,18 @@
     let
       #This is only valid for Mac installations
       system = "aarch64-darwin";
-      dioxus-0-7 = final: prev:
+      dioxus-0-7-overlay = final: prev:
         let
-          addRustPatches = pkg: patches: cargoHash:
-            pkg.overrideAttrs (oldAttrs: rec {
-              # take the original source and apply all patches before making it the new source
-              # we cannot use patches or patchPhase because all dependencies are vendored into
-              # a separate derivation before the patch phase resulting in mismatching Cargo.lock
-              # checksums
-              # (Not sure about this, original version specified a new hash, but
-              # simple code updates don't seem to need it.)
-              src = prev.runCommand "patched-source" { } ''
-                cp -v --no-preserve=mode -r ${oldAttrs.src} $out
-                cd $out
-                ${prev.lib.concatMapStringsSep "\n"
-                (patch: ''patch -p1 < "${patch}"'') patches}
-              '';
-              cargoDeps = oldAttrs.cargoDeps.overrideAttrs
-                (prev.lib.const { inherit src; });
-            });
-          cli =
+          upstream =
             nixpkgs-unstable.legacyPackages.${prev.stdenv.hostPlatform.system}.dioxus-cli;
         in {
-          dioxus-cli = addRustPatches cli [ ./patches/extra_files.patch ] "";
+          dioxus-cli = upstream.overrideAttrs (oldAttrs: {
+            patches = oldAttrs.patches ++ [ ./patches/extra_files.patch ];
+          });
         };
-      dioxus-local = final: prev: {
-        dioxus-cli = final.stdenv.mkDerivation {
-          name = "dioxus-cli";
-          __noChroot = true;
-          buildCommand = ''
-            mkdir -p $out/bin
-            cd $out/bin
-            ln -s "/Users/neil/projects/dx/target/debug/dx" dx
-          '';
-        };
-      };
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ dioxus-0-7 dioxus.overlays.default ];
+        overlays = [ dioxus-0-7-overlay dioxus.overlays.default ];
       };
     in {
       devShells.${system}.default = let applyDioxus = dioxus.addToShell pkgs;
