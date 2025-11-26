c522c47c49bfaffe5e4d5a3b1288c584c85e619a
Neil Darach <neil.darach@gmail.com>
2025-11-26 13:20:13 +0000

    Edit the player name by tapping on it

--- Scoreboard/src/main.rs
+++ Scoreboard/src/main.rs
@@ -28,31 +28,58 @@ fn App() -> Element {
     */
     let p1 = use_memo(move || (SCORES.read().player_1).clone());
     let p2 = use_memo(move || (SCORES.read().player_2).clone());
-    fn update_score(player: Player, action: Action) {
+    fn update(player: Player, action: Action) {
         SCORES.write().update(player, action);
     }
 
     rsx! {
         document::Stylesheet { href: CSS }
-        div { transform: "rotate(180deg)", Counter { player: p2, update_score: move |d| update_score(Player::PlayerTwo,Action::ChangeScore(d))} }
+        div { transform: "rotate(180deg)", Counter { player: p2, update: move |a| update(Player::PlayerTwo,a)} }
         Reset { onclick: move |_| { SCORES.write().update(Player::PlayerOne,Action::ResetScore); SCORES.write().update(Player::PlayerTwo,Action::ResetScore)}}
-        div { Counter { player: p1, update_score: move |d| update_score(Player::PlayerOne,Action::ChangeScore(d)) } }
+        div { Counter { player: p1, update: move |a| update(Player::PlayerOne,a) } }
 
     }
 }
 
 #[component]
-fn Counter(player: Memo<CribScore>, update_score: EventHandler<i16>) -> Element {
+fn Counter(player: Memo<CribScore>, update: EventHandler<Action>) -> Element {
+    let mut editing_title = use_signal(|| false);
+    let id = use_signal(|| uuid::Uuid::new_v4().to_string());
+    let ds_name = use_memo(move || if editing_title() { "none" } else { "block" });
+    let ds_input = use_memo(move || if editing_title() { "block" } else { "none" });
+    let set_focus = move |i| async move {
+        let js = format!(r#" document.getElementById("{}").focus() "#, i);
+        let _ = dioxus::document::eval(&js).await;
+    };
+    use_effect(move || {
+        let editing = editing_title();
+        let i = id().clone();
+        if editing {
+            spawn(set_focus(i));
+        };
+    });
+    let pname = if player().name.is_empty() {
+        "Anon"
+    } else {
+        &player().name
+    };
     rsx! {
-           div { class: "title", "{player().name}" }
+           div { class: "title", display: ds_name, onclick: move |_| { editing_title.set(true); },
+                 "{pname}" }
+           div { class: "title", display: ds_input,
+                 input { id,
+                         autocomplete: false, autocorrect: false, autocapitalize: false, spellcheck: false,
+                         oninput: move |e| update.call(Action::ChangeName(e.value().clone())),
+                         onkeydown: move |e| if e.key() == Key::Enter { editing_title.set(false); },
+                         onfocusout: move |_| editing_title.set(false) } }
            br { }
            span { class: "prevscore", "{player().previous_score}" }
            span { class: "score", "{player().score}"}
            br { }
-           button { class: "action", onclick: move |_| update_score.call(10), "+10" }
-           button { class: "action", onclick: move |_| update_score.call(5), "+5" }
-           button { class: "action", onclick: move |_| update_score.call(1), "+1" }
-           button { class: "action", onclick: move |_| update_score.call(-1), "-1" }
+           button { class: "action", onclick: move |_| update.call(Action::ChangeScore(10)), "+10" }
+           button { class: "action", onclick: move |_| update.call(Action::ChangeScore(5)), "+5" }
+           button { class: "action", onclick: move |_| update.call(Action::ChangeScore(1)), "+1" }
+           button { class: "action", onclick: move |_| update.call(Action::ChangeScore(-1)), "-1" }
     }
 }
 
